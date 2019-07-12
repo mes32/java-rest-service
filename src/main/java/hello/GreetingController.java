f060730f7a221f79c577c38a145fc7d810825a3c
mes32 <stockman.mike@gmail.com>
2019-07-12 09:51:47 -0500

    Fix parameter formatting problem in GreetingController

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")) String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template,name));
+                String.format(template, name));
     }
 }
